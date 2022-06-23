aa729a286e513cd69cb55550f18703fc8f5d31f4
HuongNan <phamthihuonglan234@gmail.com>
2022-06-23 18:52:50 +0700

    test

--- Admin/js/home.js
+++
@@ -1,5 +0,0 @@
-adminName = sessionStorage.getItem('name')
-adminPass = sessionStorage.getItem('password')
-if(adminName != 'admin' || adminPass != 'admin'){
-    window.location = './admin-login.html'
-}
